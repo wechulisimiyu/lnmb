d50a2c1f9ab562c9c85182f53d3df8de2a401d9f
Dennis Kidake <denniskidake76@gmail.com>
2025-08-22 13:16:07 +0300

    chore: update Next.js config to turbopack (codemod)

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 // next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    turbo: {
-      root: __dirname, // set Turbopack root to this project
-    },
+  turbopack: {
+    // set Turbopack root to this project
+    root: __dirname
   },
 };
 

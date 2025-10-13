361beb8ef8ba9f3e5e4220af8fa9dabbb50fb17a
Wechuli Simiyu <wechuli017@gmail.com>
2025-10-13 09:57:08 +0300

    Refactor RegisterPage to comment out TallyOrderForm and ensure OrderForm

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,12 +1,14 @@
 import ShopHero from "@/components/shop/hero";
-import TallyOrderForm from "@/components/shop/tally-order-form";
+import OrderForm from "@/components/shop/order-form";
+// import TallyOrderForm from "@/components/shop/tally-order-form";
 
 export default function RegisterPage() {
   return (
     <div className="py-8 sm:py-12 lg:py-16 bg-slate-50 min-h-screen">
       <div className="container mx-auto px-4">
         <ShopHero />
-        <TallyOrderForm />
+        {/* <TallyOrderForm /> */}
+        <OrderForm />
       </div>
     </div>
   );

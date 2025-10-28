8975d7d81789c17a5259b112253cc122c5c38083
Wechuli Simiyu <wechuli017@gmail.com>
2025-10-28 13:39:31 +0300

    feat: Add global CSS module declarations and update tsconfig include paths

---
+++ src/global.d.ts
@@ -0,0 +1,9 @@
+declare module '*.css';
+declare module '*.scss';
+declare module '*.module.css';
+declare module '*.module.scss';
+
+// Removed export to make declarations ambient
+declare module './globals.css';
+
+export {};
